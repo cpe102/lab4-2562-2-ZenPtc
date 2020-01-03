2d736dfecee2efe77bce61704747cdf701153896
ZenPtc <maku4fb@gmail.com>
2020-01-03 15:58:21 +0700

    lab4.3

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int findDivisor(int x){
+    int n=2;
+    while(x%n !=0){
+        n++;
+    }
+    return n;
+}
+
+int main(){
+
+    int x;
+    cout << "Input number: ";
+    cin >> x;
+
+    cout << "Divisor is " << findDivisor(x);
+
+    return 0;
+}
