4887d37e3b22f73848a8c4d163c2bbdac0f01b97
ZenPtc <maku4fb@gmail.com>
2020-01-03 15:07:46 +0700

    lab4.1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+float findY(float x){
+    float y= 3*pow(x,3) + 2*2.71828 + pow(2,(2*x+1)) + sqrt(pow(x,2)+1);
+    return y;
+}
+
+int main(){
+
+    float x;
+    cout << "Enter x = ";
+    cin >> x;
+    cout << "Result y = " << findY(x);
+
+    return 0;
+}
